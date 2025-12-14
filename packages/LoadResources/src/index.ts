4dd2248f7b6cf855e4ad47b11256e7ff91588280
dongguacute <gudupao2022@gmail.com>
2025-12-14 20:23:29 +0800

    feat(LoadResources): add file loading and splitting functionality
    Implement file loading using fs and path modules, and split content by specified part. This enables dynamic resource loading instead of hardcoded initialization.

--- packages/LoadResources/src/index.ts
+++ packages/LoadResources/src/index.ts
@@ -1,3 +1,8 @@
-export const loadResources = () => {
-  console.log('LoadResources module initialized');
+import * as fs from "fs";
+import * as path from "path";
+
+export const loadResources = (filename: string, part: string) => {
+  const filePath = path.resolve(__dirname, filename);
+  const fileContent = fs.readFileSync('./content'+filePath, 'utf-8');
+  return fileContent.split(part);
 };
