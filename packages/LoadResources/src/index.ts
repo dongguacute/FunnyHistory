65da3083df68f12270c8aed3a208ecbf151c1782
dongguacute <gudupao2022@gmail.com>
2025-12-14 20:49:30 +0800

    refactor(LoadResources): rename parameter 'part' to 'number' for clarity
    The parameter was renamed to better reflect its purpose of splitting content by part numbers. The splitting logic was also updated to use a more explicit format.

--- packages/LoadResources/src/index.ts
+++ packages/LoadResources/src/index.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 import * as path from "path";
 
-export const loadResources = (filename: string, part: string) => {
+export const loadResources = (filename: string, number: string) => {
   const filePath = path.resolve(__dirname, filename);
   const fileContent = fs.readFileSync('./content'+filePath, 'utf-8');
-  return fileContent.split(part);
+  return fileContent.split('part-'+'-'+number);
 };
